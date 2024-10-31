c0f7ac30438176b074bd01f67f73509ced7b6a42
aya <eng.ayashawky123@gmail.com>
2024-10-31 12:19:17 +0300

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:tododayapp_p/models/task-provider.dart';
 import 'package:tododayapp_p/screens/TasksScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
